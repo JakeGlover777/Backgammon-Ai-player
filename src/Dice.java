eb974792912fbae60d8c51720a0f93697153f6ec
Jake Glover <jakeglover777@gmail.com>
2026-07-26 14:43:52 -0400

    Created dice class
    Added method for 2 dice rolls between 1-6 and implemented the backgammon rule of a doubleroll.

--- src/Dice.java
+++ src/Dice.java
@@ -1,3 +1,42 @@
+import java.util.Random;
+
 public class Dice
 {
+    private int dieOne;
+    private int dieTwo;
+    private final Random random;
+
+    public Dice()
+    {
+        this.random = new Random();
+        this.dieOne = 0;
+        this.dieTwo = 0;
+    }
+
+    public void roll()
+    {
+        dieOne = random.nextInt(6) + 1;
+        dieTwo = random.nextInt(6) + 1;
+    }
+
+    public int getDieOne()
+    {
+        return dieOne;
+    }
+
+    public int getDieTwo()
+    {
+        return dieTwo;
+    }
+
+    public boolean isDouble()
+    {
+        return dieOne == dieTwo;
+    }
+
+    @Override
+    public String toString()
+    {
+        return "Die 1: " + dieOne + " | Die 2: " + dieTwo;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,22 @@ public class Main
 {
     public static void main(String[] args)
     {
+        /*
         Board board = new Board();
-
         board.printBoard();
+        */
+
+        Dice dice = new Dice();
+
+        for (int i = 0; i < 10; i++)
+        {
+            dice.roll();
+            System.out.println(dice);
+
+            if (dice.isDouble())
+            {
+                System.out.println("Double rolled!");
+            }
+        }
     }
 }
-
-
