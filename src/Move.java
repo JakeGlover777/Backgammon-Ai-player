fa2979e8f9fdf0337c7dadd70a4ce5ebca2e2e17
Jake Glover <jakeglover777@gmail.com>
2026-07-27 21:21:17 -0400

    Created move class
    Implemented immutable move model and game actions

--- src/Move.java
+++ src/Move.java
@@ -1,3 +1,61 @@
 public class Move
 {
+    private final Player player;
+    private final int fromPoint;
+    private final int toPoint;
+    private final int dieValue;
+    private final boolean bearingOff;
+    private static final int BEAR_OFF = -1;
+
+    public Move(Player player, int fromPoint, int toPoint, int dieValue)
+    {
+        this.player = player;
+        this.fromPoint = fromPoint;
+        this.toPoint = toPoint;
+        this.dieValue = dieValue;
+        this.bearingOff = false;
+    }
+
+    public Move(Player player, int fromPoint, int dieValue)
+    {
+        this.player = player;
+        this.fromPoint = fromPoint;
+        this.toPoint = BEAR_OFF;
+        this.dieValue = dieValue;
+        this.bearingOff = true;
+    }
+
+
+    public Player getPlayer()
+    {
+        return player;
+    }
+
+    public int getFromPoint()
+    {
+        return fromPoint;
+    }
+
+    public int getToPoint()
+    {
+        return toPoint;
+    }
+
+    public int getDieValue()
+    {
+        return dieValue;
+    }
+
+    public boolean isBearingOff()
+    {
+        return bearingOff;
+    }
+
+    @Override
+    public String toString()
+    {
+        return player + ": " + fromPoint + " -> " + toPoint +
+                " (Die: " + dieValue + ")" +
+                (bearingOff ? " Bearing Off" : "");
+    }
 }
